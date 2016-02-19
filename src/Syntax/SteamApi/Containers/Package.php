6a91c03340dab5d9fce00f28b0758f3e9b154e44
M1st4M <gamekeymonkey@gmail.com>
2016-02-19 21:49:24 +0100

    Create Package.php

---
+++ src/Syntax/SteamApi/Containers/Package.php
@@ -0,0 +1,43 @@
+<?php namespace Syntax\SteamApi\Containers;
+
+use Syntax\SteamApi\Collection;
+
+class Package extends BaseContainer {
+	
+	public $id;
+
+	public $name;
+
+	public $apps;
+
+	public $header;
+
+	public $page_image;
+
+	public $price;
+
+	public $platforms;
+
+	public $release;
+
+	public function __construct($package)
+	{
+		$this->name               = $package->name;
+		$this->apps               = $package->apps;
+		$this->small_logo         = $this->checkIssetField($package, 'small_logo', 'none');
+		$this->header             = $this->checkIssetField($package, 'header_image', 'none');
+		$this->page_image         = $this->checkIssetField($package, 'page_image', 'none');
+		$this->price              = $this->checkIssetField($package, 'price_overview', $this->getFakePriceObject());
+		$this->platforms          = $package->platforms;
+		$this->release            = $package->release_date;
+	}
+
+	protected function getFakePriceObject()
+	{
+		$object        		= new \stdClass();
+		$object->initial 	= '0';
+		$object->final 		= '0';
+		$object->individual = '0';
+
+		return $object;
+	}
